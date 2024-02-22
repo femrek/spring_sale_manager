0316ac17339338ce77126ea010aebead4bb74ae9
Faruk Emre EKŞİOĞLU <faruk.eksioglu@hotmail.com>
2024-02-22 10:14:57 +0300

    feat: sale service created and registered to gateway.

---
+++ sale/src/test/java/dev/faruk/sale/SaleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.faruk.sale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
