964ec491ce26885a857e322bc67df40918ea15f0
Faruk Emre EKŞİOĞLU <faruk.eksioglu@hotmail.com>
2024-04-09 23:53:29 +0300

    test: report module service test with test repositories.

---
+++ common-codebase/src/main/java/dev/faruk/commoncodebase/repository/testImplementations/UserTestRepository.java
@@ -0,0 +1,135 @@
+package dev.faruk.commoncodebase.repository.testImplementations;
+
+import dev.faruk.commoncodebase.entity.AppUser;
+import dev.faruk.commoncodebase.entity.AppUserRole;
+import dev.faruk.commoncodebase.repository.base.UserRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class UserTestRepository implements UserRepository {
+    private final List<AppUser> cache = new ArrayList<>();
+    private long idCounter = 1;
+
+    List<AppUserRole> roles = List.of(
+            AppUserRole.builder()
+                    .id(1L)
+                    .name("ADMIN")
+                    .build(),
+            AppUserRole.builder()
+                    .id(2L)
+                    .name("CASHIER")
+                    .build(),
+            AppUserRole.builder()
+                    .id(3L)
+                    .name("MANAGER")
+                    .build()
+    );
+
+    @Override
+    public List<AppUser> findAll() {
+        return cache;
+    }
+
+    @Override
+    public List<AppUser> findAllOnlyExist() {
+        return cache.stream().filter(AppUser::getDeleted).toList();
+    }
+
+    @Override
+    public List<AppUser> findAllCashiers() {
+        return cache.stream().filter(user -> user.getRoles().stream()
+                .anyMatch(role -> role.getName().equals("CASHIER"))).toList();
+    }
+
+    @Override
+    public AppUser findByUsername(String username) {
+        return cache.stream().filter(user -> user.getUsername().equals(username)).findFirst().orElse(null);
+    }
+
+    @Override
+    public AppUser findById(Long id) {
+        return cache.stream().filter(user -> user.getId().equals(id)).findFirst().orElse(null);
+    }
+
+    @Override
+    public AppUser findOnlyExistByUsername(String username) {
+        return cache.stream().filter(user -> user.getUsername().equals(username) && user.getDeleted())
+                .findFirst().orElse(null);
+    }
+
+    @Override
+    public AppUser findOnlyExistById(Long id) {
+        return cache.stream().filter(user -> user.getId().equals(id) && user.getDeleted())
+                .findFirst().orElse(null);
+    }
+
+    @Override
+    public AppUser create(AppUser user) {
+        user.setId(idCounter++);
+        cache.add(user);
+        return user;
+    }
+
+    @Override
+    public AppUser update(AppUser user) {
+        int index = cache.stream().map(AppUser::getId).toList().indexOf(user.getId());
+        if (index != -1) {
+            cache.set(index, user);
+            return user;
+        }
+        throw new IllegalArgumentException("User not found");
+    }
+
+    @Override
+    public void deleteByUsername(String username) {
+        cache.stream().filter(user -> user.getUsername().equals(username)).findFirst().ifPresentOrElse(
+                cache::remove,
+                () -> {
+                    throw new IllegalArgumentException("User not found");
+                }
+        );
+    }
+
+    @Override
+    public void deleteSoftByUsername(String username) {
+        cache.stream().filter(user -> user.getUsername().equals(username)).findFirst().ifPresentOrElse(user -> {
+            user.setDeleted(true);
+            update(user);
+        }, () -> {
+            throw new IllegalArgumentException("User not found");
+        });
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        cache.stream().filter(user -> user.getId().equals(id)).findFirst().ifPresentOrElse(
+                cache::remove, () -> {
+                    throw new IllegalArgumentException("User not found");
+                }
+        );
+    }
+
+    @Override
+    public void deleteSoftById(AppUser user) {
+        user.setDeleted(true);
+        update(user);
+    }
+
+    @Override
+    public List<AppUserRole> findRoles() {
+        return roles;
+    }
+
+    @Override
+    public AppUserRole findRoleById(Long id) {
+        return roles.stream().filter(role -> role.getId().equals(id)).findFirst().orElse(null);
+    }
+
+    @Override
+    public AppUserRole findRoleCashier() {
+        return roles.stream().filter(role -> role.getName().equals("CASHIER")).findFirst().orElse(null);
+    }
+}
---
+++ report/src/test/java/dev/faruk/report/service/ReportServiceTest.java
@@ -0,0 +1,222 @@
+package dev.faruk.report.service;
+
+import dev.faruk.commoncodebase.dto.SaleDTO;
+import dev.faruk.commoncodebase.entity.AppUser;
+import dev.faruk.commoncodebase.entity.AppUserRole;
+import dev.faruk.commoncodebase.entity.Sale;
+import dev.faruk.commoncodebase.repository.base.SaleRepository;
+import dev.faruk.commoncodebase.repository.base.UserRepository;
+import dev.faruk.report.ReportTestConfigurer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(classes = ReportTestConfigurer.class)
+class ReportServiceTest {
+    private final SaleRepository saleRepository;
+    private final UserRepository userRepository;
+    private final ReportService reportService;
+
+    @Autowired
+    public ReportServiceTest(@Qualifier("testSaleRepository") SaleRepository saleRepository,
+                             @Qualifier("testUserRepository") UserRepository userRepository,
+                             @Qualifier("testReportService") ReportService reportService) {
+        this.saleRepository = saleRepository;
+        this.userRepository = userRepository;
+        this.reportService = reportService;
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @BeforeEach
+    public void setUpEach() {
+        // reset sales
+        List<Sale> sales = saleRepository.findAll(
+                null,
+                null,
+                null,
+                true,
+                null,
+                null,
+                null,
+                null,
+                null
+        );
+        final int saleSize = sales.size();
+        for (int i = saleSize - 1; i >= 0; i--) {
+            saleRepository.deletePermanent(sales.get(i));
+        }
+
+        // reset users
+        List<AppUser> users = userRepository.findAll();
+        final int userSize = users.size();
+        for (int i = userSize - 1; i >= 0; i--) {
+            userRepository.deleteByUsername(users.get(i).getUsername());
+        }
+    }
+
+    @Test
+    public void reportService_whenShowSalesCalled_thenReturnsSavedSales() {
+        addSaleSet0();
+        List<SaleDTO> sales = reportService.showSales(
+                1,
+                100,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null
+        );
+        assertEquals(2, sales.size());
+    }
+
+    @Test
+    public void reportService_whenShowSalesCalledWithOrderBy_thenReturnsSavedSalesInOrder() {
+        addSaleSet0();
+        List<SaleDTO> sales = reportService.showSales(
+                1,
+                100,
+                "receivedMoney",
+                null,
+                null,
+                null,
+                null,
+                null,
+                null
+        );
+        assertEquals(100D, sales.get(0).getReceivedMoney());
+    }
+
+    @Test
+    public void reportService_whenShowSalesCalledWithCashierFilter_thenReturnsSavedSalesByCashier() {
+        addSaleSet0();
+        AppUser sampleCashier = userRepository.findByUsername("sampleCashier");
+        List<SaleDTO> sales = reportService.showSales(
+                1,
+                100,
+                null,
+                null,
+                null,
+                null,
+                sampleCashier.getId(),
+                null,
+                null
+        );
+        assertEquals(2, sales.size());
+    }
+
+    @Test
+    public void reportService_whenShowSalesCalledWithNotExistCashierFilter_thenReturnsNoSale() {
+        addSaleSet0();
+        userRepository.create(AppUser.builder()
+                .username("sampleCashier2")
+                .name("Sample Cashier 2")
+                .password("sampleCashierPassword2")
+                .roles(List.of(userRepository.findRoleCashier()))
+                .build());
+        AppUser sampleCashierWithNotSale = userRepository.findByUsername("sampleCashier2");
+        List<SaleDTO> sales = reportService.showSales(
+                1,
+                100,
+                null,
+                null,
+                null,
+                null,
+                sampleCashierWithNotSale.getId(),
+                null,
+                null
+        );
+        userRepository.deleteByUsername("sampleCashier2");
+        assertEquals(0, sales.size());
+    }
+
+    @Test
+    public void reportService_whenShowSalesCalledWithReceivedMoneyFilter_thenReturnsSavedSalesByReceivedMoney() {
+        addSaleSet0();
+        List<SaleDTO> sales = reportService.showSales(
+                1,
+                100,
+                null,
+                null,
+                null,
+                null,
+                null,
+                100D,
+                150D
+        );
+        assertEquals(1, sales.size());
+        assertEquals(100D, sales.get(0).getReceivedMoney());
+    }
+
+    @Test
+    public void reportService_whenShowSalesCalledWithPagination_thenReturnsPaginatedSales() {
+        addSaleSet0();
+        List<SaleDTO> sales = reportService.showSales(
+                1,
+                1,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null
+        );
+        assertEquals(1, sales.size());
+    }
+
+    @Test
+    public void reportService_whenShowSaleCalled_thenReturnsSale() {
+        addSaleSet0();
+        List<SaleDTO> sales = reportService.showSales(
+                1,
+                100,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null
+        );
+        Long saleId = sales.get(0).getId();
+        SaleDTO saleFromService = reportService.showSale(saleId);
+        assertEquals(saleId, saleFromService.getId());
+    }
+
+    private void addSaleSet0() {
+        AppUserRole cashierRole = userRepository.findRoleCashier();
+        AppUser sampleCashier = AppUser.builder()
+                .username("sampleCashier")
+                .name("Sample Cashier")
+                .password("sampleCashierPassword")
+                .roles(List.of(cashierRole))
+                .build();
+        Sale sale0 = Sale.builder()
+                .receivedMoney(200D)
+                .cashier(sampleCashier)
+                .productList(List.of())
+                .build();
+        Sale sale1 = Sale.builder()
+                .receivedMoney(100D)
+                .cashier(sampleCashier)
+                .productList(List.of())
+                .build();
+        userRepository.create(sampleCashier);
+        saleRepository.create(sale0);
+        saleRepository.create(sale1);
+    }
+}
